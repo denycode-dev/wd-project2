06b49c8e1eed47bd9d9eb0ad08bbdc57df01c2e2
Deni Irawan Nugraha <deni.irawan40563@gmail.com>
2022-06-12 13:31:21 +0700

    ini

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     // host: '192.168.136.46' // default: localhost
   },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -76,5 +76,9 @@ export default {
       description: 'Undangan digital pernikahan Ferian Cahyana dan  Listiana Hikmah',
       short_name: 'F & L'
     }
+  },
+
+  performance: {
+    hints: false
   }
 }
